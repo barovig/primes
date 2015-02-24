35acd265f61a273d0458ab1c1961421ab2a741d9
Roman <semargl1349@gmail.com>
2015-02-24 22:30:14 +0000

    q1 finished

---
+++ Primes_CA/FactorList/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<long> list = PrimeCheck(100);
+            foreach(var num in list)
+                Console.Write(num+" ");
+
+            Console.ReadKey();
+        }
+        static List<long> PrimeCheck(long n)
+        {
+            List<long> list = new List<long>();
+            int sqrt = (int)Math.Floor(Math.Sqrt((n)));
+            for (long k = 2; k < sqrt; ++k)
+            {
+                if(n % k == 0)
+                {
+                    long newN = n;
+                    while (newN % k == 0)
+                    {
+                        list.Add(k);
+                        newN /= k;
+                    }
+                }
+            }
+
+            return list;
+        }
+    }
+}
---
+++ Primes_CA/Primes_CA/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0} is {1}", 30, Convert.ToBoolean(PrimeCheck(30)) ? "prime" : "not prime");
+            Console.WriteLine("{0} is {1}", 31, Convert.ToBoolean(PrimeCheck(31)) ? "prime" : "not prime");
+            Console.WriteLine("{0} is {1}", 487, Convert.ToBoolean(PrimeCheck(487)) ? "prime" : "not prime");
+            Console.WriteLine("{0} is {1}", 8893, Convert.ToBoolean(PrimeCheck(8893)) ? "prime" : "not prime");
+            Console.WriteLine("{0} is {1}", 987654323, Convert.ToBoolean(PrimeCheck(987654323)) ? "prime" : "not prime");
+            Console.WriteLine("{0} is {1}", 131317171919, Convert.ToBoolean(PrimeCheck(131317171919)) ? "prime" : "not prime");
+
+            Console.Read();
+        }
+
+        public static int PrimeCheck(long n)
+        {
+            // calculate square root estimate
+            int sqrt = (int)Math.Floor(Math.Sqrt((n)));
+            for (int i = 2; i <= sqrt; ++i)
+            {
+                if (n % i == 0) // factor found
+                    return 0;
+            }
+            return 1;
+        }
+    }
+}
